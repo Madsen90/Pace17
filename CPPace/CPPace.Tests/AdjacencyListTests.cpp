e16144b94db005f6c69391f7057f8142ba530df6
Malthe Ettrup Kirkbro <maek@itu.dk>
2017-05-22 13:05:23 +0200

    Remove AdjacencyList benchmarks
    These are being run with all test runs, causing a huge delay.
    Reintroduce when we can better seperate the tests.

--- CPPace/CPPace.Tests/AdjacencyListTests.cpp
+++ CPPace/CPPace.Tests/AdjacencyListTests.cpp
@@ -108,21 +108,4 @@ namespace CPPaceTests {
       Assert::IsTrue(graph.is_clique(set<int> { 0, 1, 4 }));
     }
   };
-
-  TEST_CLASS(AdjacencyListBench) {
-  public:
-    TEST_METHOD(AddEdgeRandom) {
-      const unsigned int seed = 0x69c384b2;
-      const int num_edges = 1000000;
-      const int num_vertices = 1000;
-      AdjacencyList graph(num_vertices);
-      srand(seed);
-      
-      for (size_t i = 0; i < num_edges; i++) {
-        int u = rand() % num_vertices;
-        int v = rand() % num_vertices;
-        graph.add_edge(u, v);
-      }
-    }
-  };
 }
