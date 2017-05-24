c0eafd65cb09fbef430fc989880335e18e33848e
Malthe Ettrup Kirkbro <maek@itu.dk>
2017-05-24 16:06:17 +0200

    Move test graph generators to GraphGenerator

--- CPPace/CPPace.Lib/GraphGenerator.cpp
+++ CPPace/CPPace.Lib/GraphGenerator.cpp
@@ -13,5 +13,43 @@ AdjacencyList GraphGenerator::random(int num_vertices, int num_edges) {
   }
 
   return graph;
+}
 
+AdjacencyList GraphGenerator::fully_connected(int n) {
+  //Fully connected with N nodes
+  AdjacencyList graph(n);
+  for (int i = 0; i < n; i++) {
+    for (int j = 1; j < n; j++) {
+      graph.add_edge(i, j);
+    }
+  }
+  return graph;
+}
+
+AdjacencyList GraphGenerator::half_connected(int n) {
+  //Fully connected with N nodes
+  AdjacencyList graph(n);
+  for (int i = 0; i < n; i++) {
+    for (int j = 1; j < n; j++) {
+      graph.add_edge(i, j);
+    }
+  }
+  srand(0);
+  for (int i = 0; i < n; i++) {
+    graph.vertices[i].active = (rand() % 2);
+  }
+
+  return graph;
+}
+
+AdjacencyList GraphGenerator::cycle(int n) {
+  //Cycle with N nodes
+  AdjacencyList graph(n);
+  for (int i = 0; i < n; i++) {
+    if (i == n - 1)
+      graph.add_edge(i, 0);
+    else
+      graph.add_edge(i, i + 1);
+  }
+  return graph;
 }
--- CPPace/CPPace.Lib/GraphGenerator.h
+++ CPPace/CPPace.Lib/GraphGenerator.h
@@ -8,4 +8,7 @@ using namespace std;
 class DLLEXPORT GraphGenerator {
 public:
   static AdjacencyList random(int num_vertices, int num_edges);
+  static AdjacencyList fully_connected(int n);
+  static AdjacencyList half_connected(int n);
+  static AdjacencyList cycle(int n);
 };
