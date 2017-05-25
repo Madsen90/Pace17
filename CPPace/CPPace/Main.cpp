6d00cb85928495187e4838ce8693e4ee6c465af0
Malthe Ettrup Kirkbro <maek@itu.dk>
2017-05-25 16:01:35 +0200

    Optil main ifdef

--- CPPace/CPPace/Main.cpp
+++ CPPace/CPPace/Main.cpp
@@ -5,17 +5,18 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  switch (argc) {
-  case 1: {
-    GraphIO::GraphContext context = GraphIO::read_from_stream(cin);
-    stack<pair<int, int>> edges = MinimumFillIn::minimum_fill_in(context);
-    while (!edges.empty()) {
-      pair<int, int> edge = edges.top();
-      edges.pop();
-      cout << context.vertex_name(edge.first) << " " << context.vertex_name(edge.second) << endl;
-    }
-    break;
+#ifdef OPTIL
+  GraphIO::GraphContext context = GraphIO::read_from_stream(cin);
+  stack<pair<int, int>> edges = MinimumFillIn::minimum_fill_in(context);
+  while (!edges.empty()) {
+    pair<int, int> edge = edges.top();
+    edges.pop();
+    cout << context.vertex_name(edge.first) << " " << context.vertex_name(edge.second) << endl;
   }
+  exit(0);
+#endif
+  
+  switch (argc) {
   case 2: {
     GraphIO::GraphContext context = GraphIO::read_from_path(argv[1]);
     Log::info("NAME: .......... %s", argv[1]);
